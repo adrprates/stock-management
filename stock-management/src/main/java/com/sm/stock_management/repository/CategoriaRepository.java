e3bc6a26dc984963484f8ed2c9aa1aab6d87b71f
Adriano Prates <adrianopratessouza@gmail.com>
2024-11-29 16:38:06 -0300

    Criando método de busca pelo nome em `CategoriaRepository.java`

--- stock-management/src/main/java/com/sm/stock_management/repository/CategoriaRepository.java
+++ stock-management/src/main/java/com/sm/stock_management/repository/CategoriaRepository.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.sm.stock_management.repository;
+
+import com.sm.stock_management.model.Categoria;
+import java.util.List;
 
 /**
  *
  * @author Adriano
  */
 public interface CategoriaRepository {
-    
+    List<Categoria> findByNameContaining(String nome);
 }
