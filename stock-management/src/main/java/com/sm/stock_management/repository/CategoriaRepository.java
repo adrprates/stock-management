1a56fcaaaefe387d11afc38c0311c2cac475e558
Adriano Prates <adrianopratessouza@gmail.com>
2024-11-29 16:48:53 -0300

    Corrigindo `CategoriaRepository.java`

--- stock-management/src/main/java/com/sm/stock_management/repository/CategoriaRepository.java
+++ stock-management/src/main/java/com/sm/stock_management/repository/CategoriaRepository.java
@@ -2,11 +2,12 @@ package com.sm.stock_management.repository;
 
 import com.sm.stock_management.model.Categoria;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author Adriano
  */
-public interface CategoriaRepository {
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
     List<Categoria> findByNameContaining(String nome);
 }
