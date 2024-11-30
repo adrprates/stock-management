ef181f1a35f63ae2916a505cbeb61a4139b981ff
Adriano Prates <adrianopratessouza@gmail.com>
2024-11-30 15:17:45 -0300

    Criando classe `ProdutoService.java` no pacote `service`

---
+++ stock-management/src/main/java/com/sm/stock_management/service/ProdutoService.java
@@ -0,0 +1,53 @@
+package com.sm.stock_management.service;
+
+import com.sm.stock_management.model.Produto;
+import com.sm.stock_management.repository.ProdutoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Adriano
+ */
+
+@Service
+public class ProdutoService {
+    
+    @Autowired
+    ProdutoRepository produtoRepository;
+    
+    public Produto adicionar(Produto produto){
+        produto.setId(null);
+        produtoRepository.save(produto);
+        return produto;
+    }
+    
+    public Produto buscarPorId(Integer id){
+        return produtoRepository.findById(id).orElseThrow();
+    }
+    
+    public List<Produto> buscarTodas(){
+        return produtoRepository.findAll();
+    }
+    
+    public List<Produto> buscarPorNome(String nome){
+        return produtoRepository.findByNomeContaining(nome);
+    }
+    
+    public Produto atualizar(Integer id, Produto produto){
+        Produto produtoEncontrado = buscarPorId(id);
+        
+        produtoEncontrado.setNome(produto.getNome());
+        
+        produtoRepository.save(produtoEncontrado);
+        return  produtoEncontrado;
+    }
+    
+    public void excluir(Integer id){
+        Produto produtoEncontrado = buscarPorId(id);
+        
+        produtoRepository.deleteById(produtoEncontrado.getId());
+    }
+    
+}
