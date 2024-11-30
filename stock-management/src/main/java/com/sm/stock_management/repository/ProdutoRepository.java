472a3754e25286044a7a2f11a96f6b8ff2cf741b
Adriano Prates <adrianopratessouza@gmail.com>
2024-11-30 15:12:27 -0300

    Configurando `ProdutoRepository.java`

--- stock-management/src/main/java/com/sm/stock_management/repository/ProdutoRepository.java
+++ stock-management/src/main/java/com/sm/stock_management/repository/ProdutoRepository.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.sm.stock_management.repository;
+
+import com.sm.stock_management.model.Produto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Adriano
  */
-public interface ProdutoRepository {
-    
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    List<Produto> findByNomeContaining(String nome);
 }
