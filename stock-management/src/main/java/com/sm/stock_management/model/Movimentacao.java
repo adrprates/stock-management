3c179a1ed95b5322657f80ea44a468e05839ca22
Adriano Prates <adrianopratessouza@gmail.com>
2024-12-01 11:02:14 -0300

    Criando classe `Movimentacao` no pacote `model`

---
+++ stock-management/src/main/java/com/sm/stock_management/model/Movimentacao.java
@@ -0,0 +1,104 @@
+package com.sm.stock_management.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalTime;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ *
+ * @author Adriano
+ */
+
+@Entity
+@Table(name="Movimentacoes")
+public class Movimentacao {
+    
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+    
+    private int quantidade;
+    private String tipo;
+    
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date data;
+    
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime hora;
+    
+    //construtor vazio
+    public Movimentacao(){
+        
+    }
+
+    //construtor
+    public Movimentacao(Integer id, Produto produto, int quantidade, String tipo, Date data, LocalTime hora) {
+        this.id = id;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.tipo = tipo;
+        this.data = data;
+        this.hora = hora;
+    }
+
+    //getters e setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+}
