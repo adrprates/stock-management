1531b0efc58fb6355c57217a4b05d44575f5409b
Adriano Prates <adrianopratessouza@gmail.com>
2024-12-02 15:14:07 -0300

    Criando classe `UsuarioService.java` no pacote `service`

---
+++ stock-management/src/main/java/com/sm/stock_management/service/UsuarioService.java
@@ -0,0 +1,52 @@
+package com.sm.stock_management.service;
+
+import com.sm.stock_management.model.Usuario;
+import com.sm.stock_management.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Adriano
+ */
+
+@Service
+public class UsuarioService {
+    
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    
+    public Usuario adicionar(Usuario usuario){
+        usuario.setId(null);
+        usuarioRepository.save(usuario);
+        return usuario;
+    }
+    
+    public Usuario buscarPorId(Integer id){
+        return usuarioRepository.findById(id).orElseThrow();
+    }
+    
+    public List<Usuario> buscarTodos(){
+        return usuarioRepository.findAll();
+    }
+    
+    public List<Usuario> buscarPorNome(String nome){
+        return usuarioRepository.findByNomeContaining(nome);
+    }
+    
+    public Usuario atualizar(Integer id, Usuario usuario){
+        Usuario usuarioEncontrado = buscarPorId(id);
+        
+        usuarioEncontrado.setNome(usuario.getNome());
+        
+        usuarioRepository.save(usuarioEncontrado);
+        return usuarioEncontrado;
+    }
+    
+    public void excluir(Integer id){
+        Usuario usuarioEncontrado = buscarPorId(id);
+        
+        usuarioRepository.deleteById(usuarioEncontrado.getId());
+    }
+}
