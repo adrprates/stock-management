91d71228ae8b950ece08a00a66605b9108abe838
Adriano Prates <adrianopratessouza@gmail.com>
2024-12-02 15:07:26 -0300

    Criando interface `UsuarioRepository.java` no pacote `repository`

---
+++ stock-management/src/main/java/com/sm/stock_management/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.sm.stock_management.repository;
+
+import com.sm.stock_management.model.Usuario;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Adriano
+ */
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    List<Usuario> findByNomeContaining(String nome);
+}
