f7f2600032120b4dddd7c3b7b27ad611a37eb83c
Adriano Prates <adrianopratessouza@gmail.com>
2024-12-03 14:45:13 -0300

    Criando método em `UsuarioRepository.java` para verificar se o login digitado pertence a um usuário cadastrado

--- stock-management/src/main/java/com/sm/stock_management/repository/UsuarioRepository.java
+++ stock-management/src/main/java/com/sm/stock_management/repository/UsuarioRepository.java
@@ -13,4 +13,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     List<Usuario> findByNomeContaining(String nome);
+    boolean existsByLogin(String login);
 }
